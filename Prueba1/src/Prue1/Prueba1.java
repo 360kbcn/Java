da9063a4c6af8dd28c40dbc33f3731dcc199c665
360kbcn <360kbcn@gmail.com>
2018-06-07 09:32:08 +0200

    fecha_07/06/2018

---
+++ Prueba1/src/Prue1/Prueba1.java
@@ -0,0 +1,15 @@
+package Prue1;
+
+public class Prueba1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola Mundo");
+
+	}
+
+}
